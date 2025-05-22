ff98a8f1f7ac5e55122a1e2a5883d75605883590
Sunhyoup <kciter@naver.com>
2025-05-22 11:09:12 +0900

    Add builder package for package build

---
+++ internals/builder/rune-build.js
@@ -0,0 +1,14 @@
+// @ts-check
+import path from "node:path";
+import { build } from "./builder.js";
+
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
+
+const args = process.argv.slice(2);
+const relativePath = path.relative(process.cwd(), args[0] || ".");
+
+await build(relativePath);
