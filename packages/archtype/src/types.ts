6e77e6cd8798a3b9f17e66a61163dcc58c656711
Sunhyoup <kciter@naver.com>
2025-05-23 14:44:36 +0900

    Clean up tsconfigs

---
+++ packages/archtype/src/types.ts
@@ -0,0 +1,26 @@
+import type { View } from "rune-ts";
+
+export type RuneView =
+  | View<any>
+  | JSX.Element
+  | string
+  | number
+  | boolean
+  | null
+  | undefined;
+
+export type RuneChildren = RuneView | RuneView[];
+
+export type As =
+  | keyof HTMLElementTagNameMap
+  | (abstract new (...args: any[]) => View<any>);
+
+export type PropsOf<T extends As> = T extends keyof HTMLElementTagNameMap
+  ? Omit<HTMLElementTagNameMap[T], "children" | "style"> & {
+      style?: string;
+    }
+  : T extends abstract new (...args: any[]) => View<infer P>
+    ? P
+    : never;
+
+export type RuneElement<T extends As> = PropsOf<T> & {};
